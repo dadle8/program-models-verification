a515de813cf373db60cd4eb3d67968cc38890985
Vyacheslav Tikhonov <dadle8@yandex.ru>
2019-03-27 21:20:47 +0300

    Create util class for generate graph image

--- task2/src/main/java/tk/dadle8/program/models/verification/Task2.java
+++ task2/src/main/java/tk/dadle8/program/models/verification/Task2.java
@@ -1,29 +1,8 @@
 package tk.dadle8.program.models.verification;
 
-import guru.nidi.graphviz.attribute.Label;
-import guru.nidi.graphviz.attribute.RankDir;
-import guru.nidi.graphviz.attribute.Shape;
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
-import guru.nidi.graphviz.model.Graph;
-
-import java.io.File;
-import java.io.IOException;
-
-import static guru.nidi.graphviz.model.Factory.*;
-
 public class Task2 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Task2");
-
-        Graph g = graph("example1").directed()
-                .graphAttr().with(RankDir.LEFT_TO_RIGHT)
-                .with(
-                        node("a").with(Shape.RECTANGLE).link(node("b")),
-                        node("b").link(node("c").with(Label.lines("a = b;", "a > b"))),
-                        node("c").link(node("a"))
-                );
-        Graphviz.fromGraph(g).height(100).basedir(new File("C:/.")).render(Format.PNG).toFile(new File("task2/out/out.png"));
     }
 }
---
+++ task2/src/main/java/tk/dadle8/program/models/verification/graph/GraphUtils.java
@@ -0,0 +1,36 @@
+package tk.dadle8.program.models.verification.graph;
+
+import guru.nidi.graphviz.attribute.Label;
+import guru.nidi.graphviz.attribute.RankDir;
+import guru.nidi.graphviz.attribute.Shape;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
+
+import java.io.File;
+import java.io.IOException;
+
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.node;
+
+public class GraphUtils {
+
+    public void generateImage() {
+        Graph g = graph("example1").directed()
+                .graphAttr().with(RankDir.LEFT_TO_RIGHT)
+                .with(
+                        node("a").with(Shape.RECTANGLE).link(node("b")),
+                        node("b").link(node("c").with(Label.lines("a = b;", "a > b"))),
+                        node("c").link(node("a"))
+                );
+
+        try {
+            Graphviz.fromGraph(g).height(100).basedir(new File("C:/."))
+                    .render(Format.PNG)
+                    .toFile(new File("task2/out/out.png"));
+        } catch (IOException exception) {
+            exception.getStackTrace();
+        }
+
+    }
+}
