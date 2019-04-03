a3e079f9c6ca669452d1bb3b9329e00e515fcfcb
dadle8 <dadly8@gmail.com>
2019-04-03 08:14:40 +0300

    Refactor methods for generating tree

--- task1/src/main/java/tk/dadle8/program/models/verification/task1/Task1.java
+++ task1/src/main/java/tk/dadle8/program/models/verification/task1/Task1.java
@@ -1,28 +1,20 @@
 package tk.dadle8.program.models.verification.task1;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import tk.dadle8.program.models.verification.task1.antlr4.ProLangLexer;
 import tk.dadle8.program.models.verification.task1.antlr4.ProLangParser;
 import tk.dadle8.program.models.verification.task1.parser.ConcreteTreeListener;
 import tk.dadle8.program.models.verification.task1.util.ParserUtils;
 
-import java.io.IOException;
-
 public class Task1 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Task1");
 
         ParserUtils parserUtils = new ParserUtils();
 
-        ProLangLexer lexer = new ProLangLexer(CharStreams.fromStream(parserUtils.getFile("text.txt")));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ProLangParser parser = new ProLangParser(tokens);
+        ProLangParser parser = parserUtils.getParseTreeFromFile("text.txt");
         ParseTree tree = parser.source();
-
         ConcreteTreeListener listener = new ConcreteTreeListener(parser);
         ParseTreeWalker.DEFAULT.walk(listener, tree);
         parserUtils.writeFile(listener.toString(), "task1/out/result.txt");
--- task1/src/main/java/tk/dadle8/program/models/verification/task1/util/ParserUtils.java
+++ task1/src/main/java/tk/dadle8/program/models/verification/task1/util/ParserUtils.java
@@ -1,5 +1,14 @@
 package tk.dadle8.program.models.verification.task1.util;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.Parser;
+import tk.dadle8.program.models.verification.task1.antlr4.ProLangBaseListener;
+import tk.dadle8.program.models.verification.task1.antlr4.ProLangLexer;
+import tk.dadle8.program.models.verification.task1.antlr4.ProLangParser;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,14 +21,24 @@ public class ParserUtils {
     }
 
     public void writeFile(String text, String pathToFile) {
-        try(FileWriter writer = new FileWriter(pathToFile, false))
-        {
+        try (FileWriter writer = new FileWriter(pathToFile, false)) {
             writer.write(text);
             writer.flush();
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
 
             System.out.println(ex.getMessage());
         }
+    }
+
+    public ProLangParser getParseTreeFromFile(String fileFrom) {
+        ProLangParser parser = null;
+        try {
+            ProLangLexer lexer = new ProLangLexer(CharStreams.fromStream(this.getFile(fileFrom)));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            parser = new ProLangParser(tokens);
+        } catch (IOException exc) {
+            exc.getStackTrace();
+        }
+        return parser;
     }
 }
